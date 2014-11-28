bb16d52254c0b06d60a72b00b5d7e0fdf406cc6e
rdanieldesign <rdanieldesign@gmail.com>
2014-11-28 13:49:16 -0500

    Geolocation data successful

--- app/scripts/factories/create_factory.js
+++ app/scripts/factories/create_factory.js
@@ -8,12 +8,36 @@
 		var catchURL = 'https://api.parse.com/1/classes/catches/';
 
 		var file;
+		var geo;
 
 		$('#imageFile').bind("change", function(e) {
 			var files = e.target.files || e.dataTransfer.files;
 			// Our file var now holds the selected file
 			file = files[0];
+
+			EXIF.getData(file, function() {
+				console.log(this);
+				var aLat = EXIF.getTag(this, 'GPSLatitude');
+				var aLong = EXIF.getTag(this, 'GPSLongitude');
+				if (aLat && aLong) {
+					var latRef = EXIF.getTag(this, 'GPSLatitudeRef') || 'N';
+					var longRef = EXIF.getTag(this, 'GPSLongitudeRef') || 'W';
+					var fLat = (aLat[0].numerator + aLat[1].numerator/aLat[1].denominator + aLat[2].numerator/aLat[1].denominator) * (latRef === 'N' ? 1 : -1);
+					var fLong = (aLong[0].numerator + aLong[1].numerator/ aLong[1].denominator + aLong[2].numerator/aLong[1].denominator) * (longRef === 'W' ? -1 : 1);
+
+					geo = {
+						latitude: fLat,
+						longitude: fLong,
+						latitudeRef: latRef,
+						longitudeRef: longRef
+					};
+				}
+				else {
+					console.log('failure');
+				}
+			});
 		});
+
 
 		var getCatches = function(){
 			return $http.get(catchURL, P_HEADERS);
@@ -36,9 +60,10 @@
 			})
 			.success( function(data){
 				fish.picURL = data.url;
+				fish.geoData = geo;
 				$http.post(catchURL, fish, P_HEADERS)
 				.success( function(){
-					console.log('catch added');
+					console.log(geo);
 				});
 			})
 			.error( function(data) {
