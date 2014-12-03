43dbcf6c571c2f2f0b7ba0f5f481e6074a20fee5
rdanieldesign <rdanieldesign@gmail.com>
2014-12-02 20:25:43 -0500

    Remove unneccesary code from user

--- app/scripts/factories/create_factory.js
+++ app/scripts/factories/create_factory.js
@@ -109,11 +109,6 @@
 						"className": "_User",
 						"objectId": currentUser.objectId
 					},
-					// "river": {
-					// 	"__type": "Pointer",
-					// 	"className": "rivers",
-					// 	"objectId": "nYPd56jbab"
-					// },
 					"status": 'draft'
 				}, P_HEADERS)
 				.success( function(data){
