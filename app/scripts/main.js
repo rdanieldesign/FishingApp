16f065f4bf2fabdce193de1d2918c0f6637d559c
rdanieldesign <rdanieldesign@gmail.com>
2014-12-06 10:39:26 -0500

    Added spinner

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -73,6 +73,17 @@
 		});
 		$rootScope.haversine = CreateFactory.haversine;
 		RiverFactory.getNSGS();
-	}]);
+	}])
+
+	.directive("loader", function ($rootScope) {
+		return function ($scope, element, attrs) {
+			$scope.$on("loader_show", function () {
+				return element.show();
+			});
+			return $scope.$on("loader_hide", function () {
+				return element.hide();
+			});
+		};
+	});
 
 }());
