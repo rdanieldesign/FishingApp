b8d322f47f030f9aa20a2b40f7d1fcfd69ec49e1
rdanieldesign <rdanieldesign@gmail.com>
2014-12-09 13:00:56 -0500

    Responsive spacing at nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -98,8 +98,10 @@
 					var st = $(this).scrollTop();
 					if (st > lastScrollTop){
 						$('nav').addClass('reducedNav');
+						$('.main').addClass('reducedMain');
 					} else if(st < lastScrollTop - 5){
 						$('nav').removeClass('reducedNav');
+						$('.main').removeClass('reducedMain');
 					}
 					lastScrollTop = st;
 				};
